7cbc6999cafcdf1a50778be3bee329650c9a87bb
Shea Munion <sheamunion@gmail.com>
2017-11-28 21:32:43 -0600

    Add passing unit test for submitting refunder form on home page.

--- refunder/tests.py
+++ refunder/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .forms import RefunderForm
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
--- refunder/views.py
+++ refunder/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import RefunderForm
+
+def index(request):
+    if request.method == 'POST':
+        form = RefunderForm(request.POST, request.FILES)
+        if form.is_valid():
+            return HttpResponseRedirect('/refunding')
+    else:
+        form = RefunderForm()
+
+    return render(request, 'home.html', {'form': form})
+
