73dfd4a341fd0c10b61aa5edca27025d79d36e0c
Shea Munion <sheamunion@gmail.com>
2017-12-03 22:47:57 -0600

    Clean up file paths. Clean up temp files after tests.

--- refunder/test_views.py
+++ refunder/test_views.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.test import TestCase
+import glob
 import os
 
 from .forms import RefunderForm
@@ -15,12 +17,19 @@ class NewRefundPageTest(TestCase):
 
 class RefundPageTest(TestCase):
 
+    def tearDown(self):
+        for f in glob.glob(os.path.join(settings.BASE_DIR, 'refunder/files/*')):
+            os.remove(f)
+
     def test_POST_to_refund_redirects_to_refunding_page(self):
-        dummy_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'functional_tests', 'dummy_source.csv')
+        dummy_file = os.path.join(settings.BASE_DIR, 'functional_tests/dummy_source.csv')
         with open(dummy_file) as fp:
             response = self.client.post('/refund', {'environment': 'sandbox', 'merchant_id': 'asdf', 'public_key': 'asdf', 'private_key': 'asdf', 'source_csv': fp})
+        
         self.assertRedirects(response, '/refunding')
     
     def test_POST_to_refund_with_invalid_data_renders_new_refund_page(self):
         response = self.client.post('/refund')
+
         self.assertIn('What type of account is this?', (response.content).decode('utf-8'))
+        self.assertIn('start_refund', (response.content).decode('utf-8'))
