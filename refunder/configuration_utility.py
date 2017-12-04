57f47fa0f728340b9e3ce70d239c6abe4655b69a
Shea Munion <sheamunion@gmail.com>
2017-12-03 22:45:20 -0600

    Remove unnecessary utility. No need to test Braintree.

--- refunder/configuration_utility.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class ConfigurationUtility:
-
-    def __init__(self, keys):
-        os.environ["BT_MERCHANT_ID"] = keys[0]
-        os.environ["BT_PUBLIC_KEY"]  = keys[1]
-        os.environ["BT_PRIVATE_KEY"] = keys[2]
-        
