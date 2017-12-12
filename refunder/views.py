2db01d4aca7f791b2208df7a9eaad604287ebc46
Shea Munion <sheamunion@gmail.com>
2017-12-11 22:45:35 -0600

    User can download a log.

--- refunder/views.py
+++ refunder/views.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, Http404
 from django.shortcuts import redirect, render
-import csv
+import csv, os
 
 from .forms import RefunderForm
 from refunder.refund_job import RefundJob
@@ -9,10 +12,11 @@ def new_refund(request):
     return render(request, 'home.html', {'form': form})
 
 def start_refund(request):
-    error = False
+    error = None
     if request.method == 'POST':
         form = RefunderForm(request.POST, request.FILES)
         if form.is_valid():
+            error = None 
             keys = [
                 form.cleaned_data['environment'], 
                 form.cleaned_data['merchant_id'], 
@@ -22,14 +26,24 @@ def start_refund(request):
             decoded_file = form.cleaned_data['source_csv'].read().decode('utf-8').splitlines()
             try:	
                 job = RefundJob(keys, decoded_file)
-                job.run()
-                return redirect('/refunding')
+                log_file_name = job.run(keys[1])
+                return redirect(reverse('refunding', args=[log_file_name]))
             except TypeError as e:
-                error = "API keys are incorrect."
+                error = e
     else: 
         form = RefunderForm()
     return render(request, 'home.html', {'form': form, 'error': error})
 
-def refunding(request):
-    return render(request, 'refunding.html')
+def refunding(request, file_name):
+    context = {'file_name': file_name}
+    return render(request, 'refunding.html', context )
+
+def download(request, file_name):
+    file_path = '{}/refunder/files/{}'.format(settings.BASE_DIR, file_name)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename={}'.format(file_name)
+            return response 
+    raise Http404
 
