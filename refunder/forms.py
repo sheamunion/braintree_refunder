5f54220d58774c345d9dc9a337ffbe13c6b762cb
Shea Munion <sheamunion@gmail.com>
2017-12-14 10:56:27 -0600

    Make senstive input fields of type password.

--- refunder/forms.py
+++ refunder/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class RefunderForm(forms.Form):
     ENV_CHOICES = (('production', 'Production',),('sandbox','Sandbox',))
     environment = forms.ChoiceField(widget=forms.RadioSelect, choices=ENV_CHOICES)
-    merchant_id = forms.CharField(label='Merchant ID', max_length=32)
-    public_key  = forms.CharField(label='Public Key', max_length=64)
-    private_key = forms.CharField(label='Private Key', max_length=128)
+    merchant_id = forms.CharField(label='Merchant ID', max_length=32, widget=forms.PasswordInput())
+    public_key  = forms.CharField(label='Public Key', max_length=64, widget=forms.PasswordInput())
+    private_key = forms.CharField(label='Private Key', max_length=128, widget=forms.PasswordInput())
     source_csv  = forms.FileField()
